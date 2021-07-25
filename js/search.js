083443af45caad89dc864954910f92a06a3caa82
vitorarrud4 <arruda.gif@gmail.com>
2021-07-25 18:33:56 -0300

    Atualizacao final

--- js/search.js
+++ js/search.js
@@ -1,30 +1,27 @@
 const resPesquisa = document.querySelector(".resultadoPesquisa");
 const barraPesquisa = document.querySelector(".barraPesquisa");
 
+const resultadoPesquisa = document.querySelector(".resultadoPesquisa");
+document.querySelector(".botaoPesquisa").addEventListener("click", function () {
+  resultadoPesquisa.innerHTML = "";
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "https://aws.random.cat/meow", true);
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+      for (var i = 0; i < barraPesquisa.value; i++) {
+        cat = JSON.parse(xmlhttp.responseText);
+        var li = document.createElement("li");
+        var img = document.createElement("img");
+        img.style.width = "200px";
+        li.innerHTML = "Gato >> " + i + " ";
 
-const resultadoPesquisa = document.querySelector(".resultadoPesquisa")
-document.querySelector(".botaoPesquisa").addEventListener("click", function() {
-    resultadoPesquisa.innerHTML = ""
-    for(var i = 0; i < barraPesquisa.value; i++) {
-        var li = document.createElement('li') 
-        var img = document.createElement('img')
-        img.style.width = "200px"
-        li.innerHTML = "gato" + i + " "
-
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", "https://aws.random.cat/meow", true);
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === 4 &&  xmlhttp.status === 200) {
-                cat = JSON.parse(xmlhttp.responseText)
-                img.src = cat.file
-                li.appendChild(img)
-                resultadoPesquisa.appendChild(li)
-            } else {
-                console.log("Erro ao acessar a API")
-                // console.log(xmlhttp.status)
-                // console.log(xmlhttp.readyState)
-            }
-        }
-        xmlhttp.send()
+        img.src = cat.file;
+        li.appendChild(img);
+        resultadoPesquisa.appendChild(li);
+      }
+    } else {
+      console.log("Erro ao acessar a API");
     }
-})
+  };
+  xmlhttp.send();
+});
